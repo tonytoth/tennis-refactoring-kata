fedd695e6230c59153fd8cb68624cbb7267b5eeb
Mierlut Timotei <mierluttimotei@Mierluts-MacBook-Pro.local>
2023-12-18 17:49:27 +0200

    I've reworked all if statements by trying to get rid of the one liners and I reversed some of the logic (the nesting could be better though). I've also added a 'finalScore' var for more consistent and readable returns.

--- src/TennisGame3.ts
+++ src/TennisGame3.ts
@@ -11,9 +11,18 @@ export class TennisGame3 implements TennisGame {
     this.player2Name = player2Name;
   }
 
+  findLeadingPlayer(): string {
+    if (this.player1Points > this.player2Points) {
+      return this.player1Name;
+    }
+    return this.player2Name
+  }
+
   getScore(): string {
     let player1Score: string;
     let player2Score: string;
+    let finalScore: string;
+    const leadingPlayer: string = this.findLeadingPlayer();
 
     const scoringNames: string[] = 
     [
@@ -23,24 +32,39 @@ export class TennisGame3 implements TennisGame {
       'Forty'     // 3 points
     ];
 
+    //if conditions
     const playerPointsAreLessThan4 = this.player1Points < 4 && this.player2Points < 4;
     const thereIsNoDeuce = !(this.player1Points + this.player2Points === 6);
+    const playerHasAdvantage = (this.player1Points - this.player2Points) * (this.player1Points - this.player2Points) === 1;
+    
     if (playerPointsAreLessThan4 && thereIsNoDeuce) {
       player1Score = scoringNames[this.player1Points];
       player2Score = scoringNames[this.player2Points];
-      return (this.player1Points === this.player2Points) ? player1Score + '-All' : player1Score + '-' + player2Score;
+      if (this.player1Points != this.player2Points){
+        finalScore = player1Score + '-' + player2Score;
+        return finalScore;
+      }
+      finalScore = player1Score + '-All';
+      return finalScore;
     } else {
-      if (this.player1Points === this.player2Points)
-        return 'Deuce';
-      const leadingPlayer = this.player1Points > this.player2Points ? this.player1Name : this.player2Name;
-      return (((this.player1Points - this.player2Points) * (this.player1Points - this.player2Points)) === 1) ? 'Advantage ' + leadingPlayer : 'Win for ' + leadingPlayer;
+      if (this.player1Points === this.player2Points) {
+        finalScore = 'Deuce'
+        return finalScore;
+      }
+      if (playerHasAdvantage) {
+        finalScore = 'Advantage ' + leadingPlayer;
+        return finalScore;
+      }
+      finalScore = 'Win for ' + leadingPlayer;
+      return finalScore;
     }
   }
 
   wonPoint(playerName: string): void {
-    if (playerName === 'player1')
+    if (playerName === 'player1') {
       this.player1Points += 1;
-    else
-      this.player2Points += 1;
+      return;
+    }
+    this.player2Points +=1 ;
   }
 }
