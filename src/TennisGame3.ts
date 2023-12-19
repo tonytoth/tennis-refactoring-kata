71749bc83a50019846d50580e6c1208393e8b929
Mierlut Timotei <mierluttimotei@Mierluts-MacBook-Pro.local>
2023-12-19 11:37:58 +0200

    I have privatized almost everything, just like america

--- src/TennisGame3.ts
+++ src/TennisGame3.ts
@@ -1,30 +1,14 @@
 import { TennisGame } from './TennisGame';
 
 export class TennisGame3 implements TennisGame {
-  private player1Points: number = 0;
-  private player2Points: number = 0;
-  private player1Name: string;
-  private player2Name: string;
-
-  constructor(player1Name: string, player2Name: string) {
-    this.player1Name = player1Name;
-    this.player2Name = player2Name;
-  }
-
-  findLeadingPlayer(): string {
-    if (this.player1Points > this.player2Points) {
-      return this.player1Name;
-    }
-    return this.player2Name
-  }
-
-  getScore(): string {
-    let player1Score: string;
-    let player2Score: string;
-    let finalScore: string;
-    const leadingPlayer: string = this.findLeadingPlayer();
-
-    const scoringNames: string[] = 
+  private _player1Points: number = 0;
+  private _player2Points: number = 0;
+  private _player1Score: string = '';
+  private _player2Score: string = '';
+  private _finalScore: string = '';
+  private _player1Name: string;
+  private _player2Name: string;
+  private _scoringNames: string[] = 
     [
       'Love',     // 0 points
       'Fifteen',  // 1 point
@@ -32,39 +16,54 @@ export class TennisGame3 implements TennisGame {
       'Forty'     // 3 points
     ];
 
+  constructor(player1Name: string, player2Name: string) {
+    this._player1Name = player1Name;
+    this._player2Name = player2Name;
+  }
+
+  private _findLeadingPlayer(): string {
+    if (this._player1Points > this._player2Points) {
+      return this._player1Name;
+    }
+    return this._player2Name
+  }
+
+  getScore(): string {
+    const leadingPlayer: string = this._findLeadingPlayer();
+
     //if conditions
-    const playerPointsAreLessThan4 = this.player1Points < 4 && this.player2Points < 4;
-    const thereIsNoDeuce = !(this.player1Points + this.player2Points === 6);
-    const playerHasAdvantage = (this.player1Points - this.player2Points) * (this.player1Points - this.player2Points) === 1;
+    const playerPointsAreLessThan4 = this._player1Points < 4 && this._player2Points < 4;
+    const thereIsNoDeuce = !(this._player1Points + this._player2Points === 6);
+    const playerHasAdvantage = (this._player1Points - this._player2Points) * (this._player1Points - this._player2Points) === 1;
     
     if (playerPointsAreLessThan4 && thereIsNoDeuce) {
-      player1Score = scoringNames[this.player1Points];
-      player2Score = scoringNames[this.player2Points];
-      if (this.player1Points != this.player2Points){
-        finalScore = player1Score + '-' + player2Score;
-        return finalScore;
+      this._player1Score = this._scoringNames[this._player1Points];
+      this._player2Score = this._scoringNames[this._player2Points];
+      if (this._player1Points != this._player2Points){
+        this._finalScore = this._player1Score + '-' + this._player2Score;
+        return this._finalScore;
       }
-      finalScore = player1Score + '-All';
-      return finalScore;
+      this._finalScore = this._player1Score + '-All';
+      return this._finalScore;
     } else {
-      if (this.player1Points === this.player2Points) {
-        finalScore = 'Deuce'
-        return finalScore;
+      if (this._player1Points === this._player2Points) {
+        this._finalScore = 'Deuce'
+        return this._finalScore;
       }
       if (playerHasAdvantage) {
-        finalScore = 'Advantage ' + leadingPlayer;
-        return finalScore;
+        this._finalScore = 'Advantage ' + leadingPlayer;
+        return this._finalScore;
       }
-      finalScore = 'Win for ' + leadingPlayer;
-      return finalScore;
+      this._finalScore = 'Win for ' + leadingPlayer;
+      return this._finalScore;
     }
   }
 
   wonPoint(playerName: string): void {
     if (playerName === 'player1') {
-      this.player1Points += 1;
+      this._player1Points += 1;
       return;
     }
-    this.player2Points +=1 ;
+    this._player2Points +=1 ;
   }
 }
